cc2c56929e5d8b0e4a2debd39e027c3aed814272
Seretse Mosoeu <smosoeu@c4r5s8.wethinkcode.co.za>
2020-06-23 12:17:20 +0200

    Day02 Exercises

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,23 @@
+
+
+void ft_putchar(char c)
+	
+
+void ft_print_alphabet(void) {
+
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z') {
+		ft_putchar(letter);
+		letter++;
+	}
+}
+
+int main(void) {
+	ft_print_alphabet();
+
+
+}
+
